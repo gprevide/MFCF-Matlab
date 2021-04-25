2580c073c0c81f8deef6f8b336f3d1319dec82e2
gprevide <gprevide@gmail.com>
2021-04-25 15:55:49 +0200

    Fixed issue in formatting for first_clique

--- src/MFCF.m
+++ src/MFCF.m
@@ -1,3 +1,4 @@
+ 
 function [ cliques, separators, peo, tree ] = MFCF( X, ct_control, gain_function )
 %MFCF Builds a clique forest from data
 % See:
@@ -38,12 +39,12 @@ end
 
 % get first ct_control.min_clique nodes as the first clique
 [~,j]=sort(sums,'descend');
-first_clique = j(1:ct_control.min_clique_size);
+first_clique = j(1:ct_control.min_clique_size).';
 clique_no = 1;
 cliques(clique_no, 1:numel(first_clique)) = first_clique;
 % remove first clique form outstanding_nodes
 outstanding_nodes = outstanding_nodes(~ismember(outstanding_nodes, first_clique));
-peo = first_clique;
+peo = first_clique.';
 % calculate gains for this clique
 %gf = @(v) gain_function(X, first_clique, v, ct_control);
 %[nodes gains seps] = arrayfun(gf, outstanding_nodes.', 'UniformOutput', false);
@@ -54,7 +55,7 @@ to   = from + new_gains - 1;
 GT.tot_records = GT.tot_records + new_gains;
 idx = (from:to).';
 clq_len = numel(first_clique);
-GT.cliques(idx, 1:clq_len) = repmat(first_clique.',new_gains,1);
+GT.cliques(idx, 1:clq_len) = repmat(first_clique,new_gains,1);
 if clq_len < ct_control.max_clique_size
     GT.cliques(idx, (clq_len+1):end) = NaN;
 end
@@ -70,7 +71,7 @@ while(~isempty(outstanding_nodes))
    the_sep = the_sep(~isnan(the_sep));
    parent_clique = GT.cliques(idx,:);
    parent_clique_id = id_from_set(cliques, parent_clique);
-   new_clique = [ the_sep the_node];
+   new_clique = [the_sep the_node];
    outstanding_nodes = outstanding_nodes(outstanding_nodes ~= the_node);
    peo = [peo; the_node];
    
@@ -81,6 +82,14 @@ while(~isempty(outstanding_nodes))
      %clique_to_update = clique_no;
      %cliques(clique_to_update, 1:numel(new_clique)) = new_clique;
    end
+   
+##   cl = new_clique
+##   npeo = numel(peo)
+##   noutn = numel(outstanding_nodes)
+##   pcid = parent_clique_id
+   
+   % track if it is a clique extension or a new clique
+   clique_extension = 0;
    
    % case new clique
    if numel(new_clique) <= sum(~isnan(parent_clique))
@@ -94,7 +103,11 @@ while(~isempty(outstanding_nodes))
    else
        clique_to_update = parent_clique_id;
        cliques(clique_to_update, 1:numel(new_clique)) = new_clique;
+       clique_extension = 1;
+       % parent clique is the same as the clique without the last estension
+       old_clique_idx = id_from_set(GT.cliques, parent_clique);
    end
+   
    
    % don't update the gain table when nodes finished
    if isempty(outstanding_nodes) 
@@ -102,8 +115,6 @@ while(~isempty(outstanding_nodes))
    end;
     
    % finally update gain table
-  % gf = @(v) gain_function(X, new_clique, v, ct_control);
-   %[nodes gains seps] = arrayfun(gf, outstanding_nodes.', 'UniformOutput', false);
    [nodes gains seps] = gain_function(X, new_clique, outstanding_nodes, ct_control);
    new_gains = numel(gains);
    from = GT.tot_records + 1;
@@ -119,12 +130,17 @@ while(~isempty(outstanding_nodes))
    GT.gains(idx) = gains;
    GT.separators(idx, :) = seps;
    GT.nodes(idx)= nodes;
-   % remove from gain table where clique is the old one and node is the new
-   % one
+   
+   % remove from gain table where node is the new one
    idx = find(GT.nodes == the_node);
    GT.gains(idx) = NaN;
-   %idx = id_from_set(GT.cliques, parent_clique);
-   %GT.gains(idx) = NaN;
+   
+   % if the clique was expanded remove the records with the old clique from the 
+   % gain table
+   if clique_extension == 1
+     GT.gains(old_clique_idx) = NaN;
+   end
+
    % if drop sep remove also the separator just used
    if ct_control.drop_sep == true
        idx = id_from_set(GT.separators, the_sep);
